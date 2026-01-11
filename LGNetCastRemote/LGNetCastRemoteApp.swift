c86d8df8c259b2fb4137fa5b4220f79059b2172f
Deniss Mihhailov <krasava1081@gmail.com>
2026-01-11 22:25:55 +0200

    Initial Commit

---
+++ LGNetCastRemote/LGNetCastRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  LGNetCastRemoteApp.swift
+//  LGNetCastRemote
+//
+//  Created by Deniss Mihhailov on 11.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct LGNetCastRemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
